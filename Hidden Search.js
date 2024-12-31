587cb2df7c334c8842f715a71fb7e075bb86d6a8
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-12-31 10:31:27 +0100

    Commit

---
+++ Hidden Search.js
@@ -0,0 +1,24 @@
+
+// 4. Hidden Search Widget
+
+// Important concepts for the project
+
+// 1. querySelector()
+// 2. addEventListener('click')
+// 3. element.classList.toggle()
+// 4. focus()
+
+
+// variables
+const search = document.querySelector('.search');
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+
+// event listener on button with function to toggle active class
+
+btn.addEventListener('click', function(){
+    // toggle of active
+    search.classList.toggle('active');
+    // place input focus class to get cursor on input
+    input.focus();
+});
